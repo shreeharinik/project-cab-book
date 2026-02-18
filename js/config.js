24678d0a69fca5a3324dc09263a783f8c4fca6a3
shree <159869108+shreeharinik@users.noreply.github.com>
2026-02-18 04:34:45 +0000

    Update AWS Cognito configuration values

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_imsGA3i89xx', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '15lppdlcnhshf3dakrg6ef1beh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_DwYJAIXvg', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5kctgct0r7eagjtkhf5jbp07bd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
